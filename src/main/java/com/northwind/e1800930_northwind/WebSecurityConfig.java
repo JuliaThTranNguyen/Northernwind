c1e37ab0baa5119f713e624012243b93e29dff75
e1800930 <e1800930@edu.vamk.fi>
2020-02-19 21:07:39 +0200

    bugs fixed and Swagger added

--- src/main/java/com/northwind/e1800930_northwind/WebSecurityConfig.java
+++ src/main/java/com/northwind/e1800930_northwind/WebSecurityConfig.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,23 +16,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.authorizeRequests()
-				.anyRequest().fullyAuthenticated()
+			.csrf().disable()
+			.authorizeRequests()
+				.anyRequest().permitAll()
 				.and()
-				.formLogin();
+			.formLogin();
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-				.ldapAuthentication()
-				.userSearchFilter("uid={0}")
-				.contextSource()
-				.url("ldap://ldap.puv.fi:389/dc=puv,dc=fi");
+			.ldapAuthentication()
+		        .userSearchFilter("uid={0}")
+		        .contextSource()
+		        .url("ldap://ldap.puv.fi:389/dc=puv,dc=fi");
 	}
-
+	
 	/*old version using application.properties and spring.ldap.embedded.ldif=classpath:test-server.ldif
-		 @Override
+	 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.ldapAuthentication()
@@ -43,7 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					.passwordEncoder(new LdapShaPasswordEncoder())
 					.passwordAttribute("userPassword");
 	}
-
 	@Bean
 	public DefaultSpringSecurityContextSource contextSource() {
 		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
